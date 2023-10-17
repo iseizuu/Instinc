2aca99c333537cf031b65a0b032a8849abe5fb94
Aizuu <64743287+iseizuu@users.noreply.github.com>
2023-10-17 22:24:29 +0700

    Add project files.

---
+++ Instincsharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Instincsharp.config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instincsharp
+{
+    internal class Program
+    {
+        private static DiscordClient Instinc { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new Reader();
+            await jsonReader.ReadJSON();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Instinc = new DiscordClient(discordConfig);
+
+            Instinc.Ready += Instinc_Ready;
+            await Instinc.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task Instinc_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Instincsharp/config/Reader.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instincsharp.config
+{
+    internal class Reader
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+        public async Task ReadJSON()
+        {
+            using (StreamReader sr = new StreamReader("config.json"))
+            {
+                string json = await sr.ReadToEndAsync();
+                JSONreader data = JsonConvert.DeserializeObject<JSONreader>(json);
+
+                this.token = data.token;
+                this.prefix = data.prefix;
+            }
+        }
+    }
+    internal sealed class JSONreader
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+    }
+
+}
